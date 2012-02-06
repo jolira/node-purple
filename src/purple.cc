8d5f7be413425134a3a2281e1c64f3c9011c8c98
Joachim Kainz <jfk@jolira.com>
2012-02-06 15:26:57 -0800

    greatly simplified API.

--- src/purple.cc
+++ src/purple.cc
@@ -3,29 +3,31 @@
 #include <purple.h>
 
 using namespace v8;
+static Persistent<Function> notifier;
 
-const unsigned short CORE_UI_OPS_COUNT=3;
-static Persistent<Function> coreUICallbacks[CORE_UI_OPS_COUNT];
-
-static void runCoreCallback(unsigned short idx) {
-    Handle<Function> cb = coreUICallbacks[idx];
-    Local<Value> argv[0];
-    
-    if (!cb.IsEmpty()) {
-        cb->Call(Context::GetCurrent()->Global(), 0, argv);
-    }
+static void notify(const unsigned argc, Local<Value> argv[]) {
+    notifier->Call(Context::GetCurrent()->Global(), argc, argv);
 }
 
 static void callbackUiPrefsInit() {
-    runCoreCallback(0);
+    Local<Value> argv[] = {
+        String::New("ui_prefs_init")
+    };
+    notify(1, argv);
 }
 
 static void callbackDebugUiInit() {
-    runCoreCallback(1);
+    Local<Value> argv[] = {
+        String::New("debug_init")
+    };
+    notify(1, argv);
 }
 
 static void callbackUiInit() {
-    runCoreCallback(2);
+    Local<Value> argv[] = {
+        String::New("ui_init")
+    };
+    notify(1, argv);
 }
 
 static PurpleCoreUiOps coreUiOps = {
@@ -39,21 +41,126 @@ static PurpleCoreUiOps coreUiOps = {
     NULL
 };
 
-static Handle<Value> PurpleCoreSetUiOps(const Arguments& args) {
-    HandleScope scope;
-    
-    for(unsigned idx=0; idx<CORE_UI_OPS_COUNT; idx++) {
-        Handle<Function> callback = Local<Function>::Cast(args[0]);
+/**
+ * The following eventloop functions are used in both pidgin and purple-text. If your
+ * application uses glib mainloop, you can safely use this verbatim.
+ */
+#define PURPLE_GLIB_READ_COND  (G_IO_IN | G_IO_HUP | G_IO_ERR)
+#define PURPLE_GLIB_WRITE_COND (G_IO_OUT | G_IO_HUP | G_IO_ERR | G_IO_NVAL)
 
-        coreUICallbacks[idx] = *callback;
+typedef struct _PurpleGLibIOClosure {
+	PurpleInputFunction function;
+	guint result;
+	gpointer data;
+} PurpleGLibIOClosure;
+
+typedef struct
+{
+	PurpleAccountRequestType type;
+	PurpleAccount *account;
+	void *ui_handle;
+	char *user;
+	gpointer userdata;
+	PurpleAccountRequestAuthorizationCb auth_cb;
+	PurpleAccountRequestAuthorizationCb deny_cb;
+	guint ref;
+} PurpleAccountRequestInfo;
+
+static void purple_glib_io_destroy(gpointer data)
+{
+	g_free(data);
+}
+
+static gboolean purple_glib_io_invoke(GIOChannel *source, GIOCondition condition, gpointer data)
+{
+	PurpleGLibIOClosure* closure = (PurpleGLibIOClosure*)data;
+	PurpleInputCondition purple_cond;
+
+	if (condition & PURPLE_GLIB_READ_COND) {
+		purple_cond = PURPLE_INPUT_READ;
     }
+	//if (condition & PURPLE_GLIB_WRITE_COND)
+    else {
+		purple_cond = PURPLE_INPUT_WRITE;
+    }
+
+	closure->function(closure->data, g_io_channel_unix_get_fd(source),
+                      purple_cond);
     
-    purple_core_set_ui_ops(&coreUiOps);
+	return TRUE;
+}
+
+static guint glib_input_add(gint fd, PurpleInputCondition condition, PurpleInputFunction function,
+                            gpointer data)
+{
+	PurpleGLibIOClosure *closure = g_new0(PurpleGLibIOClosure, 1);
+	GIOChannel *channel;
+	GIOCondition cond;
+    
+	closure->function = function;
+	closure->data = data;
+    
+	if (condition & PURPLE_INPUT_READ) {
+		cond = G_IO_IN;
+    }
+	//if (condition & PURPLE_INPUT_WRITE)
+    else {
+		cond = G_IO_OUT;
+    }
+
+	channel = g_io_channel_unix_new(fd);
+	closure->result = g_io_add_watch_full(channel, G_PRIORITY_DEFAULT, cond,
+                                          purple_glib_io_invoke, closure, purple_glib_io_destroy);
+    
+	g_io_channel_unref(channel);
+	return closure->result;
+}
+
+static PurpleEventLoopUiOps glib_eventloops =
+{
+	g_timeout_add,
+	g_source_remove,
+	glib_input_add,
+	g_source_remove,
+	NULL,
+#if GLIB_CHECK_VERSION(2,14,0)
+	g_timeout_add_seconds,
+#else
+	NULL,
+#endif
+    
+	/* padding */
+	NULL,
+	NULL,
+	NULL
+};
+/*** End of the eventloop functions. ***/
+
+static Handle<Value> Initialize(const Arguments& args) {
+    HandleScope scope;
+    Handle<Function> callback = Local<Function>::Cast(args[0]);
+    
+    notifier = *callback;
+        
+	/* We do not want any debugging for now to keep the noise to a minimum. */
+	purple_debug_set_enabled(TRUE);
+    
+	/* Set the core-uiops, which is used to
+	 * 	- initialize the ui specific preferences.
+	 * 	- initialize the debug ui.
+	 * 	- initialize the ui components for all the modules.
+	 * 	- uninitialize the ui components for all the modules when the core terminates.
+	 */
+	purple_core_set_ui_ops(&coreUiOps);
+    
+	/* Set the uiops for the eventloop. If your client is glib-based, you can safely
+	 * copy this verbatim. */
+	purple_eventloop_set_ui_ops(&glib_eventloops);
     
     return scope.Close(Undefined());
 }
 
-static Handle<Value> PurpleUtilSetUserDir(const Arguments& args) {
+static Handle<Value> Start(const Arguments& args) {
     HandleScope scope;
     Local<Value> directory = args[0];
     String::Utf8Value utf8directory(directory);
@@ -63,21 +170,10 @@ static Handle<Value> PurpleUtilSetUserDir(const Arguments& args) {
     return scope.Close(Undefined());
 }
 
-static Handle<Value> PurpleDebugSetEnabled(const Arguments& args) {
-    HandleScope scope;
-    bool flag = args[0]->BooleanValue();
-    
-    purple_debug_set_enabled(flag);
-    
-    return scope.Close(Undefined());
-}
-
 void init(Handle<Object> target) {
-    target->Set(String::NewSymbol("purple_util_set_user_dir"),
-                FunctionTemplate::New(PurpleUtilSetUserDir)->GetFunction());
-    target->Set(String::NewSymbol("purple_debug_set_enabled"),
-                FunctionTemplate::New(PurpleDebugSetEnabled)->GetFunction());
-    target->Set(String::NewSymbol("purple_core_set_ui_ops"),
-                FunctionTemplate::New(PurpleCoreSetUiOps)->GetFunction());
+    target->Set(String::NewSymbol("start"),
+                FunctionTemplate::New(Start)->GetFunction());
+    target->Set(String::NewSymbol("init"),
+                FunctionTemplate::New(Initialize)->GetFunction());
 }
 NODE_MODULE(purple, init)
--- test/purple.test.js
+++ test/purple.test.js
@@ -2,48 +2,21 @@ var vows = require('vows');
 var assert = require('assert');
 var purple = require('../lib/purple');
 
+purple.on("ui_prefs_init", function() {
+  console.log("ui_prefs_init", arguments);
+});
+
+purple.on("debug_init", function() {
+  console.log("debug_init", arguments);
+});
+
+purple.on("ui_init", function() {
+  console.log("ui_init", arguments);
+});
+
 vows.describe('instant messages').addBatch({
-    'setting invalid user directory': {
-        topic: purple.util.setUserDir(),
-
-        'result is undefined': function (result) {
-            assert.equal (result, undefined);
-        }
-    },
-    'setting valid user directory': {
-        topic: purple.util.setUserDir("/does/not/need/to/exist"),
-
-        'result is undefined': function (result) {
-            assert.equal (result, undefined);
-        }
-    },
-    'enable debugging': {
-        topic: purple.debug.setEnabled(true),
-
-        'result is undefined': function (result) {
-            assert.equal (result, undefined);
-        }
-    },
-    'disable debugging': {
-        topic: purple.debug.setEnabled(false),
-
-        'result is undefined': function (result) {
-            assert.equal (result, undefined);
-        }
-    },
-    'install no ui callbacks': {
-        topic: purple.core.setUiOps(),
-
-        'result is undefined': function (result) {
-            assert.equal (result, undefined);
-        }
-    },
-    'install ui callbacks': {
-        topic: purple.core.setUiOps({
-          "ui_prefs_init" : function() { conole.log("ui_prefs_init called"); },
-          "debug_ui_init" : function() { conole.log("debug_ui_init called"); },
-          "ui_init" : function() { conole.log("ui_init called"); }
-        }),
+    'connect': {
+        topic: purple.start(),
 
         'result is undefined': function (result) {
             assert.equal (result, undefined);
